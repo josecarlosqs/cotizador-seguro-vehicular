b5b6d7264467955d01efeea81c96f32db34a280e
Jose C. Quichiz S <josec.quichiz@gmail.com>
2022-02-16 15:53:45 -0500

    Request data in the Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,7 +3,29 @@ import {ReactComponent as CharacterSvg} from '../assets/home-character.svg';
 
 import Form from '../components/form'
 
+import { getUserData } from '../store/modules/user.actions'
+
+import {
+  useEffect,
+  useState
+} from 'react'
+
+import {
+  useSelector,
+  useDispatch
+} from 'react-redux'
+
+import {
+  useNavigate
+} from "react-router-dom";
+
 function Home() {
+  const userState = useSelector(state => state.user)
+  const dispatch = useDispatch()
+
+  const [submited, setSubmited] = useState(false);
+
+  const navigate = useNavigate()
 
   const validateField = (fieldName, fieldValue, formData) => {
     switch (fieldName) {
@@ -35,8 +57,18 @@ function Home() {
   }
 
   const handleFormSubmit = formData => {
-    console.log('enviar', formData)
+    setSubmited(true);
+    dispatch(getUserData(formData));
   }
+
+  useEffect(() => {
+    if (submited && userState.data !== null) {
+      console.log(userState.data)
+      navigate('/arma-tu-plan');
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [submited, userState.data])
+  
 
   return (
     <div className="home-screen">
@@ -47,29 +79,29 @@ function Home() {
         <p className="aside__text-description">Cuentanos donde le haras<br />seguimiento a tu seguro</p>
       </aside>
       <main className="main">
-        <h2 class="main__title">Déjanos tus datos</h2>
+        <h2 className="main__title">Déjanos tus datos</h2>
 
         <Form validationFn={validateForm} onSuccess={handleFormSubmit}>
           <Form.Group>
 
-            <Form.Select name="tipo_documento">
+            <Form.Select disabled={userState.loading} name="tipo_documento">
               <option>DNI</option>
               <option>RUC</option>
             </Form.Select>
 
-            <Form.Input name="nro_documento" placeholder="Nro. de doc" type="number" />
+            <Form.Input  disabled={userState.loading} name="nro_documento" placeholder="Nro. de doc" type="number" />
 
           </Form.Group>
           
-          <Form.Input name="celular" type="number" placeholder="Celular" />
+          <Form.Input  disabled={userState.loading} name="celular" type="number" placeholder="Celular" />
         
-          <Form.Input name="placa" placeholder="Placa" />
+          <Form.Input disabled={userState.loading} name="placa" placeholder="Placa" />
           
-          <Form.Checkbox name="tyc">
+          <Form.Checkbox disabled={userState.loading} name="tyc">
             <p className='form__tyc-copy'>Acepto la <a href="#">Política de Protección de Datos Personales</a> y los <a href="#">Términos y Condiciones</a>.</p>
           </Form.Checkbox>
 
-          <Form.Button type="submit" className='fw-mobile form__send-button' loading={false}>
+          <Form.Button type="submit" className='fw-mobile form__send-button' loading={userState.loading}>
             COTÍZALO
           </Form.Button>
 

dc1510105bd109383c72c02f396f5e775ef14c9f
Jose C. Quichiz S <josec.quichiz@gmail.com>
2022-02-16 20:07:55 -0500

    Using single quotes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -17,7 +17,7 @@ import {
 
 import {
   useNavigate
-} from "react-router-dom";
+} from 'react-router-dom';
 
 function Home() {
   const userState = useSelector(state => state.user)
@@ -63,7 +63,6 @@ function Home() {
 
   useEffect(() => {
     if (submited && userState.data !== null) {
-      console.log(userState.data)
       navigate('/arma-tu-plan');
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -71,37 +70,37 @@ function Home() {
   
 
   return (
-    <div className="home-screen">
-      <aside className="aside">
-        <CharacterSvg className="aside__character" />
-        <p className="aside__text-new">¡Nuevo!</p>
-        <h1 className="aside__title">Seguro <span className="text-red-desktop">Vehicular</span><br /><span className="text-red">Tracking</span></h1>
-        <p className="aside__text-description">Cuentanos donde le haras<br />seguimiento a tu seguro</p>
+    <div className='home-screen'>
+      <aside className='aside'>
+        <CharacterSvg className='aside__character' />
+        <p className='aside__text-new'>¡Nuevo!</p>
+        <h1 className='aside__title'>Seguro <span className='text-red-desktop'>Vehicular</span><br /><span className='text-red'>Tracking</span></h1>
+        <p className='aside__text-description'>Cuentanos donde le haras<br />seguimiento a tu seguro</p>
       </aside>
-      <main className="main">
-        <h2 className="main__title">Déjanos tus datos</h2>
+      <main className='main'>
+        <h2 className='main__title'>Déjanos tus datos</h2>
 
         <Form validationFn={validateForm} onSuccess={handleFormSubmit}>
           <Form.Group>
 
-            <Form.Select disabled={userState.loading} name="tipo_documento">
+            <Form.Select disabled={userState.loading} name='tipo_documento'>
               <option>DNI</option>
               <option>RUC</option>
             </Form.Select>
 
-            <Form.Input  disabled={userState.loading} name="nro_documento" placeholder="Nro. de doc" type="number" />
+            <Form.Input  disabled={userState.loading} name='nro_documento' placeholder='Nro. de doc' type='number' />
 
           </Form.Group>
           
-          <Form.Input  disabled={userState.loading} name="celular" type="number" placeholder="Celular" />
+          <Form.Input  disabled={userState.loading} name='celular' type='number' placeholder='Celular' />
         
-          <Form.Input disabled={userState.loading} name="placa" placeholder="Placa" />
+          <Form.Input disabled={userState.loading} name='placa' placeholder='Placa' />
           
-          <Form.Checkbox disabled={userState.loading} name="tyc">
-            <p className='form__tyc-copy'>Acepto la <a href="#">Política de Protección de Datos Personales</a> y los <a href="#">Términos y Condiciones</a>.</p>
+          <Form.Checkbox disabled={userState.loading} name='tyc'>
+            <p className='form__tyc-copy'>Acepto la <a href='#'>Política de Protección de Datos Personales</a> y los <a href='#'>Términos y Condiciones</a>.</p>
           </Form.Checkbox>
 
-          <Form.Button type="submit" className='fw-mobile form__send-button' loading={userState.loading}>
+          <Form.Button type='submit' className='fw-mobile form__send-button' loading={userState.loading}>
             COTÍZALO
           </Form.Button>
 

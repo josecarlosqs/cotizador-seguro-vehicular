46e6d5d14017f6d3905f0245707e1fb2a32ff345
Jose C. Quichiz S <josec.quichiz@gmail.com>
2022-02-17 18:32:01 -0500

    list stepper mode

--- src/components/stepper.js
+++ src/components/stepper.js
@@ -6,21 +6,56 @@ import {
 } from '../hooks'
 
 import {
+  useNavigate
+} from 'react-router-dom'
+
+import {
   useState
 } from 'react'
 
 import { ReactComponent as ChevronLeft } from '../assets/ic-chevron-left.svg'
 
 function ListStepper (props) {
+  const navigate = useNavigate();
+
+  const goTo = path => e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    navigate(path);
+  }
   return (
     <div className='list-stepper'>
+      <div className='list-stepper__wrapper'>
+        {props.stepsArr.map((step, stepIx) => (
+          <div className='list-stepper__button-wrapper'>
+            <button onClick={ goTo(step.path) } className={'list-stepper__button' + (stepIx === props.currentStepIndex ? ' list-stepper__button--current' : '')}>
+              <span className='list-stepper__number'>{ stepIx + 1 }</span>
+              <span className='list-stepper__label'>{ step.name }</span>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>)
 }
 
 function BarStepper (props) {
+  const navigate = useNavigate();
+
+  const goBack = e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (props.currentStepIndex === 0) {
+      return;
+    }
+
+    navigate(props.stepsArr[props.currentStepIndex - 1].path);
+  }
+
   return (
     <div className='bar-stepper'>
-      <button className='bar-stepper__goback-button'>
+      <button onClick={goBack} className='bar-stepper__goback-button'>
         <ChevronLeft />
       </button>
       <p className="bar-stepper__label">PASO {props.currentStepIndex + 1} DE {props.stepsArr.length}</p>

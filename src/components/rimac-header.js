4947c69c6e64c018bcfb27ce15b6287f8fbd5020
Jose C. Quichiz S <josec.quichiz@gmail.com>
2022-02-17 14:26:31 -0500

    Header responsive

--- src/components/rimac-header.js
+++ src/components/rimac-header.js
@@ -2,12 +2,17 @@
 function RimacHeader () {
   return (
     <header className="rimac-header">
-      <img className="rimac-header__logo" src="img/logo.svg" alt="Rimac Seguros" />
-      <a className="rimac-header__call-us" href="tel:014116001">
-        <img src="img/icon/phone.svg" />
-        <span className="show-only-mobile">Llámanos</span>
-        <span className="show-only-desktop">(01) 411 6001</span>
-      </a>
+      <div className="rimac-header__container">
+        <img className="rimac-header__logo" src="img/logo.svg" alt="Rimac Seguros" />
+        <span className='rimac-header__call-us-wrapper'>
+          <span className="show-only-desktop">¿Tienes alguna duda?</span>
+          <a className="rimac-header__call-us" href="tel:014116001">
+            <img src="img/icon/phone.svg" />
+            <span className="show-only-mobile">Llámanos</span>
+            <span className="show-only-desktop">(01) 411 6001</span>
+          </a>
+        </span>
+      </div>
     </header>
   );
 }
